30ef9c605865b09b323badd652fd72ab24beefc0
molko <molkoback@gmail.com>
2018-10-18 19:30:08 +0300

    Post tests

--- test/test_fetcher.py
+++ test/test_fetcher.py
@@ -1,9 +1,19 @@
 from fetcher import * 
 
 import unittest
+import json
 
 class TestFetcher(unittest.TestCase):
-	pass
+	def test_post(self):
+		with open("data/post.json") as fp:
+			post = fetcher.Post(json.load(fp))
+		self.assertEqual(post.id, 74)
+		self.assertEqual(post.token_id, "b36a83701f1c3191e19722d6f90274bc1b5501fe69ebf33313e440fe4b0fe210")
+		self.assertEqual(post.time, 1539016000)
+		self.assertEqual(post.word_count, 8)
+		self.assertEqual(post.image_count, 2)
+		self.assertEqual(post.audio_count, 1)
+		self.assertEqual(post.file_count, 1)
 
 if __name__ == "__main__":
 	unittest.main()
