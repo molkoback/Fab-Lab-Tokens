83c7fa2fd99acf31a833386aeb6a6d736dc34887
molko <molkoback@gmail.com>
2018-10-22 20:45:46 +0300

    sender implementation

--- fetcher/sender.py
+++ fetcher/sender.py
@@ -5,11 +5,16 @@ class TokenSender:
 	def __init__(self, token_api_url):
 		self.url = token_api_url
 	
+	def post(self, token_id, tokens):
+		""" Token API POST request to insert tokens. """
+		data = data={
+			"token_id": token_id,
+			"tokens": tokens
+		}
+		return requests.post(self.url, data=data).json()
+	
 	def send(self, token_id, tokens):
-		""" TODO """
-		raise NotImplementedError()
-		
-		# TODO
-		
-		r = requests.post(self.url, data={})
-		return r.ok
+		""" Sends tokens to blockchain API. """
+		json = self.post(token_id, tokens)
+		if json["error"]:
+			raise Exception(json["error"])
