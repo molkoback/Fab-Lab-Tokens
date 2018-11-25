717af4627212eeaf6578a730a2dedb51b93c7cde
molko <molkoback@gmail.com>
2018-11-25 11:33:18 +0200

    Added pfile option

--- fetcher_main.py
+++ fetcher_main.py
@@ -11,6 +11,7 @@ token_api_url = "http://localhost:8080/api/"
 token_api_user = "document-fetcher"
 token_api_passwd = "password"
 fetch_interval = 10
+pfile = "fetcher.p"
 
 def init_logging():
 	level = logging.INFO
@@ -31,6 +32,7 @@ if __name__ == "__main__":
 	df = DocumentFetcher(
 		doc_api_url, token_api_url,
 		token_api_user=token_api_user,
-		token_api_passwd=token_api_passwd
+		token_api_passwd=token_api_passwd,
+		pfile=pfile
 	)
 	df.run(fetch_interval=fetch_interval)
